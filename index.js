b3b40e4aafed4fa3cec8fbd9a4e82613acaf24b6
Dean Sumitro <DSumitro@talentpath.com>
2019-09-04 10:45:15 -0700

    updating

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const router = require('./middleware/routes');
 const logger = require('./middleware/logger');
 const ErrorHandler = require('./middleware/errorHandler');
@@ -6,6 +7,7 @@ const ErrorHandler = require('./middleware/errorHandler');
 const app = express();
 
 // .use signifies middleware
+app.use(cors());
 app.use(express.json()); // built in express function that parses requests with json payloads
 app.use(logger);
 app.use(router);

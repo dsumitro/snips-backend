80d6fc7767b10ad3a6ae10b00f94c9fcbb2828f7
Dean Sumitro <DSumitro@talentpath.com>
2019-09-25 14:34:32 -0700

    added integration file from Andre

--- __tests__/integrations.test.js
+++ __tests__/integrations.test.js
@@ -1,7 +1,33 @@
 require('dotenv').config();
+const request = require('supertest');
+const app = require('../app');
+const dbInit = require('../db/init');
+const db = require('../db/index');
 
-describe('test', () => {
-  it('should run a test', () => {
-    console.log(process.env.TEST_DATABASE_URL);
+beforeAll(async () => {
+  await dbInit.createTables();
+  await dbInit.seedAuthors();
+  await dbInit.seedSnippets();
+});
+
+describe('Snippets', () => {
+  describe('GET/api/snips', () => {
+    it('should get all the snips ', async () => {
+      // test the /api/snips route
+      const response = await request(app).get('/api/snippets');
+      // expect two rows
+      expect(response.body.length).toBe(2);
+      // no errors
+      expect(response.error).toBeFalsy();
+      // matches the data directly
+      expect(response.body).toMatchSnapshot();
+      // status code should be 200
+      expect(response.status).toBe(200);
+    });
   });
+});
+
+afterAll(() => {
+  // close the db pool
+  db.end();
 });

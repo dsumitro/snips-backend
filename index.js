cc03f1138d7c12358f420a2610027776586fb242
Dean Sumitro <DSumitro@talentpath.com>
2019-09-25 14:17:48 -0700

    adding travis testing

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-const express = require('express');
-const cors = require('cors');
-const router = require('./middleware/routes');
-const logger = require('./middleware/logger');
-const ErrorHandler = require('./middleware/errorHandler');
-
-const app = express();
-
-// .use signifies middleware
-app.use(cors());
-app.use(express.json()); // built in express function that parses requests with json payloads
-app.use(logger);
-app.use(router);
-app.use(ErrorHandler);
+const app = require('./app');
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Snips server running on port 5000');
